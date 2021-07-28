2d5a697372693d18f12ca51033a04f9eb38585e8
mokul791 <ismailmokul@gmail.com>
2021-07-27 22:22:59 -0400

    included static files and part of housekeeping

--- cmd/web/handlers.go
+++ cmd/web/handlers.go
@@ -2,14 +2,18 @@ package main
 
 import (
 	"fmt"
+	"html/template"
+	"log"
 	"net/http"
+	"strconv"
 )
 
 /*
-URL Patterns		Handler			Action
-/					home			Display the home page
-/pegion				showPegion		Display a specific pegion
-/pegion/create		createPegion	Create a new pegion
+URL Patterns			Handler					Action
+/						home					Display the home page
+/pegion					showPegion				Display a specific pegion
+/pegion/create			createPegion			Create a new pegion
+/static/				http.FileServer			Serve a specific static file
 */
 
 // Define a home handler function which writes a byte slice containing
@@ -20,11 +24,36 @@ func home(w http.ResponseWriter, r *http.Request) {
 		http.NotFound(w, r)
 		return
 	}
-	w.Write([]byte("Hello from Pegion"))
+
+	files := []string{
+		"./ui/html/home.page.tmpl",
+		"./ui/html/base.layout.tmpl",
+		"./ui/html/footer.partial.tmpl",
+	}
+
+	ts, err := template.ParseFiles(files...)
+	if err != nil {
+		log.Println(err.Error())
+		http.Error(w, "Internal Server Error", http.StatusInternalServerError)
+	}
+
+	err = ts.Execute(w, nil)
+	if err != nil {
+		log.Println(err.Error())
+		http.Error(w, "Internal Server Error", http.StatusInternalServerError)
+	}
+
+	// w.Write([]byte("Hello from Pegion"))
 }
 
 func showPegion(w http.ResponseWriter, r *http.Request) {
-	w.Write([]byte("Displaying a specific pegion..."))
+	id, err := strconv.Atoi(r.URL.Query().Get("id"))
+	if err != nil || id < 1 {
+		http.NotFound(w, r)
+		return
+	}
+	fmt.Fprintf(w, "Displaying a specific perion with id %d...", id)
+	// w.Write([]byte("Displaying a specific pegion..."))
 }
 
 func createPegion(w http.ResponseWriter, r *http.Request) {
--- cmd/web/main.go
+++ cmd/web/main.go
@@ -1,11 +1,28 @@
 package main
 
 import (
+	"flag"
 	"log"
 	"net/http"
+	"os"
 )
 
 func main() {
+
+	// Define a new command-line flage with the name 'addr', a default value of '4000'
+	// and some short help text explaining what the flage controls. The value of
+	// flag will be stored in the addr variable at runtime.
+	// func String(name, value, usage) *string
+	addr := flag.String("addr", ":4000", "HTTP network address")
+
+	// Parse the flag
+	flag.Parse()
+
+	//  Logging to an external file: go run cmd/web/* >>/tmp/info.log 2>>/tmp/error.log
+	infoLog := log.New(os.Stdout, "INFO\t", log.Ldate|log.Ltime)
+
+	errorLog := log.New(os.Stdout, "ERROR\t", log.Ldate|log.Ltime|log.Lshortfile)
+
 	// Initialize a new servermux, router and then register the home function as the
 	// handler for the "/" URL pattern.
 	mux := http.NewServeMux()
@@ -13,9 +30,18 @@ func main() {
 	mux.HandleFunc("/pegion", showPegion)
 	mux.HandleFunc("/pegion/create", createPegion)
 
-	log.Println("Starting server on :4000")
+	fileServer := http.FileServer(http.Dir("./ui/static/"))
+	mux.Handle("/static/", http.StripPrefix("/static", fileServer))
+
+	srv := &http.Server{
+		Addr:     *addr,
+		ErrorLog: errorLog,
+		Handler:  mux,
+	}
+
+	infoLog.Printf("Starting server on %s", *addr)
 	// http.ListenAndServe() function starts a new web server.
-	err := http.ListenAndServe(":4000", mux)
-	log.Fatal(err)
+	err := srv.ListenAndServe()
+	errorLog.Fatal(err)
 
 }
